77df828e435686e8e1b0e4490b565b66167a3e77
Brachu Macbook <kacper.brachaczekpn@gmail.com>
2020-04-16 18:58:35 +0200

    git init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import {MatCardModule} from '@angular/material/card';
 import { DragscrollComponent } from './dragscroll/dragscroll.component';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
     AppComponent,
     DragscrollComponent
   ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    DragDropModule,
-    MatCardModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        DragDropModule,
+        MatCardModule,
+        MatGridListModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
